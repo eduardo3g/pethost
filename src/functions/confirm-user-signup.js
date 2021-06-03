4938fc9002373740dc0562541d4da5633e317bfd
eduardo3g <eduardosbrasil10@gmail.com>
2021-06-02 23:12:41 -0300

    feat: implement the confirm-user-signup lambda function

---
+++ src/functions/confirm-user-signup.js
@@ -0,0 +1,31 @@
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+
+const DocumentClient = new DynamoDB.DocumentClient();
+
+const { USERS_TABLE } = process.env;
+
+module.exports.handler = async event => {
+  const { name } = event.request.userAttributes;
+
+  if (event.triggerSource === 'PostConfirmation_ConfirmSignUp') {
+    const timestamp = new Date().toJSON();
+
+    const user = {
+      id: event.userName,
+      name,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+      deletedAt: null,
+    };
+
+    await DocumentClient.put({
+      TableName: USERS_TABLE,
+      Item: user,
+      ConditionExpression: 'attribute_not_exists(id)',
+    }).promise();
+
+    return event;
+  }
+
+  return event;
+};
