465e1d64dd37dc4bae037840e6dc788139a436fc
eduardo3g <eduardosbrasil10@gmail.com>
2021-07-04 20:10:54 -0300

    feat: add user to Cognito Group based on his role, after the post-confirmation event from Cognito is triggered

--- src/functions/confirm-user-signup.js
+++ src/functions/confirm-user-signup.js
@@ -1,11 +1,13 @@
 const DynamoDB = require('aws-sdk/clients/dynamodb');
+const CognitoServiceProvider = require('aws-sdk/clients/cognitoidentityserviceprovider');
 
 const DocumentClient = new DynamoDB.DocumentClient();
+const CognitoClient = new CognitoServiceProvider();
 
 const { USERS_TABLE } = process.env;
 
 module.exports.handler = async event => {
-  const { name, email } = event.request.userAttributes;
+  const { name, email, 'custom:role': role } = event.request.userAttributes;
 
   if (event.triggerSource === 'PostConfirmation_ConfirmSignUp') {
     const timestamp = new Date().toJSON();
@@ -14,10 +16,17 @@ module.exports.handler = async event => {
       id: event.userName,
       name,
       email,
+      role,
       createdAt: timestamp,
       updatedAt: timestamp,
       deletedAt: null,
     };
+
+    await CognitoClient.adminAddUserToGroup({
+      GroupName: role,
+      UserPoolId: event.userPoolId,
+      Username: event.userName,
+    }).promise();
 
     await DocumentClient.put({
       TableName: USERS_TABLE,
