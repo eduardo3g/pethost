2a7168f0d799e6715bcc367c7e8bff0db316ceb7
eduardo3g <eduardosbrasil10@gmail.com>
2021-07-05 17:01:18 -0300

    feat: implement lambda function to store pets on dynamodb

---
+++ src/functions/create-pet.js
@@ -0,0 +1,29 @@
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+
+const DocumentClient = new DynamoDB.DocumentClient();
+const ulid = require('ulid');
+
+const { PETS_TABLE } = process.env;
+
+module.exports.handler = async event => {
+  const { name, bio, birthDate, type } = event.arguments.petInput;
+
+  const newPet = {
+    id: ulid.ulid(),
+    ownerId: event.identity.username,
+    name,
+    bio,
+    birthDate,
+    type,
+    createdAt: new Date().toJSON(),
+    updatedAt: null,
+    deletedAt: null,
+  };
+
+  await DocumentClient.put({
+    TableName: PETS_TABLE,
+    Item: newPet,
+  }).promise();
+
+  return newPet;
+};
