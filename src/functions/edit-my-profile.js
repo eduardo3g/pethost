72859f9275867adca789b3e272edc8293a26552d
eduardo3g <eduardosbrasil10@gmail.com>
2021-09-29 16:42:14 -0300

    feat: implement Lambda resolver to the editMyProfile GraphQL query

---
+++ src/functions/edit-my-profile.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-nested-ternary */
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+
+const DocumentClient = new DynamoDB.DocumentClient();
+
+const { USERS_TABLE } = process.env;
+
+module.exports.handler = async event => {
+  try {
+    const currentUserId = event.identity.username;
+    const { name, imageUrl, bio, address, birthdate, houseImageUrls } =
+      event.arguments.newProfile;
+
+    const getUserResponse = await DocumentClient.get({
+      TableName: USERS_TABLE,
+      Key: {
+        id: currentUserId,
+      },
+    }).promise();
+
+    const user = getUserResponse.Item;
+
+    if (!user) {
+      throw new Error(`User was not found`);
+    }
+
+    if (currentUserId !== user.id) {
+      throw new Error("You're not allowed to edit someone else's profile");
+    }
+
+    if (user.role !== 'host' && typeof houseImageUrls !== 'undefined') {
+      throw new Error(
+        `Only hosts can upload house pictures, but this user is a ${user.role}`,
+      );
+    }
+
+    const params = {
+      TableName: USERS_TABLE,
+      Key: {
+        id: currentUserId,
+      },
+      UpdateExpression:
+        'set #name = :name, imageUrl = :imageUrl, bio = :bio, address = :address, birthdate = :birthdate, houseImageUrls = :houseImageUrls',
+      ExpressionAttributeNames: {
+        '#name': 'name',
+      },
+      ExpressionAttributeValues: {
+        ':name': name,
+        ':imageUrl': imageUrl || (user.imageUrl ? user.imageUrl : null),
+        ':bio': bio || (user.bio ? user.bio : null),
+        ':address': address || (user.address ? user.address : null),
+        ':birthdate': birthdate || (user.birthdate ? user.birthdate : null),
+        ':houseImageUrls':
+          houseImageUrls && houseImageUrls.length > 0
+            ? houseImageUrls
+            : typeof user.houseImageUrls !== 'undefined' &&
+              Array.isArray(user.houseImageUrls) &&
+              user.houseImageUrls.length > 0
+            ? user.houseImageUrls
+            : [],
+      },
+      ConditionExpression: 'attribute_exists(id)',
+    };
+
+    await DocumentClient.update(params).promise();
+
+    return true;
+  } catch (e) {
+    console.error({
+      errorType: 'UpdateUserError',
+      errorMessage: e.message,
+      errorDetails: e.stack,
+      error: e,
+    });
+
+    return false;
+  }
+};
